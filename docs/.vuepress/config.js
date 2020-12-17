59cb92424bc06810e660600f356a1ba6ce007984
🍋 <479083227@qq.com>
2020-12-17 17:10:28 +0800

    init

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,13 +4,20 @@ module.exports = {
     base: "/peace-ui-helper/",
     description: "基于vue开发的UI组件库",
     head: [
-      ['link', { rel: 'icon', href: './logo.png' }]
+      ['link', { rel: 'icon', href: './img/logo.png' }]
     ],
     themeConfig: {
       nav: [
-        { text: '指南', link: '/guide.html' },
-        { text: '文档', link: '/doc.html' },
-      ]
+        { text: '指南', link: '/guide' },
+        { text: '文档', link: '/doc/' },
+        { text: 'github', link: 'https://github.com/cuixiaohuan/peace-ui' },
+      ],sidebar: {
+        '/doc/': [
+           '',
+          '基础组件',
+          '数据展示',
+        ],
+    },
     },
     plugins: [
     ],
---
+++ docs/doc/README.md
@@ -0,0 +1,10 @@
+
+# 组件总览
+
+| 基础组件
+* [button](/doc/基础组件.html#cbutton)
+
+- - -
+
+数据展示
+* [list](/doc/数据展示.html#list)
