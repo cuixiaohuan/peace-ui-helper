2564816e06b229f4dd86c8bdb32308e3a2a864d1
🍋 <479083227@qq.com>
2020-12-17 11:19:04 +0800

    init

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,7 +4,7 @@ module.exports = {
     base: "/peace-ui-helper/",
     description: "基于vue开发的UI组件库",
     head: [
-      ['link', { rel: 'icon', href: `https://bpic.588ku.com/art_water_pic/19/04/29/107899ddd29a2fe084d9ea7cc89c3ecb.jpg` }]
+      ['link', { rel: 'icon', href: './logo.png' }]
     ],
     themeConfig: {
       nav: [
--- docs/README.md
+++ docs/README.md
@@ -1,6 +1,6 @@
 ---
 home: true
-heroImage: "https://bpic.588ku.com/art_water_pic/19/04/29/107899ddd29a2fe084d9ea7cc89c3ecb.jpg"
+heroImage: ./img/logo.png
 heroText: PEACE_UI
 tagline: 一款基于vue开发的UI组件库
 actionText: 快速上手 →
